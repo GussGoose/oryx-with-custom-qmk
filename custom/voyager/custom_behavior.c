514a8f493cf946f638da00e11014dc2853add263
gustavotorpedo <gustavo@torpedolabs.com>
2026-03-14 00:49:09 -0600

    Add custom behaviour injection

---
+++ custom/voyager/custom_behavior.c
@@ -0,0 +1,45 @@
+#define FLOW_TAP_TERM 100
+
+static bool is_home_row_mod(uint16_t keycode) {
+  switch (keycode) {
+    case MT(MOD_LSFT, KC_S):
+    case MT(MOD_LALT, KC_D):
+    case MT(MOD_LGUI, KC_F):
+    case ALL_T(KC_G):
+    case ALL_T(KC_H):
+    case MT(MOD_LGUI, KC_J):
+    case MT(MOD_LALT, KC_K):
+    case MT(MOD_LSFT, KC_L):
+      return true;
+    default:
+      return false;
+  }
+}
+
+static bool is_thumb_tap_hold(uint16_t keycode) {
+  switch (keycode) {
+    case LT(1, KC_BSPC):
+    case MT(MOD_LGUI, KC_ENTER):
+    case LT(2, KC_SPACE):
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+  return is_thumb_tap_hold(keycode);
+}
+
+uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_keycode) {
+
+  if (is_thumb_tap_hold(keycode)) {
+    return 0;
+  }
+
+  if (is_home_row_mod(keycode)) {
+    return FLOW_TAP_TERM;
+  }
+
+  return 0;
+}
