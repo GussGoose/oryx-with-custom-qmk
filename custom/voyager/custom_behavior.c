20ae3d0aa3d0b35c5d5d2fbbbcea6f6c66cda819
Gustavo Garcia <gustavogs2811@gmail.com>
2026-03-17 00:37:49 -0600

    Update custom_behavior.c

--- custom/voyager/custom_behavior.c
+++ custom/voyager/custom_behavior.c
@@ -1,136 +1,68 @@
 #pragma once
 
-static bool app_switch_forward_held = false;
-static bool app_switch_backward_held = false;
-static uint16_t app_switch_timer = 0;
-static bool app_switch_cmd_active = false;
+static bool is_hold_capable_key(uint16_t keycode) {
+  return
+    (keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
+    (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX);
+}
 
-#define APP_SWITCH_INITIAL_DELAY 180
-#define APP_SWITCH_REPEAT_DELAY 110
+static bool is_target_home_row_hold_key(uint16_t keycode) {
+  if (!is_hold_capable_key(keycode)) {
+    return false;
+  }
 
-static bool is_home_row_mod(uint16_t keycode) {
-  switch (keycode) {
-    case MT(MOD_LSFT, KC_S):
-    case MT(MOD_LALT, KC_D):
-    case MT(MOD_LGUI, KC_F):
-    case ALL_T(KC_G):
-    case ALL_T(KC_H):
-    case MT(MOD_LGUI, KC_J):
-    case MT(MOD_LALT, KC_K):
-    case MT(MOD_LSFT, KC_L):
+  switch (get_tap_keycode(keycode)) {
+    case KC_A:
+    case KC_S:
+    case KC_D:
+    case KC_F:
+    case KC_G:
+    case KC_H:
+    case KC_J:
+    case KC_K:
+    case KC_L:
+    case KC_SCLN:
       return true;
     default:
       return false;
   }
-}
-
-static bool is_thumb_tap_hold(uint16_t keycode) {
-  switch (keycode) {
-    case LT(1, KC_BSPC):
-    case MT(MOD_LGUI, KC_ENTER):
-    case LT(2, KC_SPACE):
-      return true;
-    default:
-      return false;
-  }
-}
-
-static void app_switcher_start(void) {
-  if (!app_switch_cmd_active) {
-    register_code(KC_LGUI);
-    app_switch_cmd_active = true;
-  }
-}
-
-static void app_switcher_stop(void) {
-  if (app_switch_cmd_active) {
-    unregister_code(KC_LGUI);
-    app_switch_cmd_active = false;
-  }
-}
-
-static void app_switcher_step_forward(void) {
-  tap_code(KC_TAB);
-}
-
-static void app_switcher_step_backward(void) {
-  register_code(KC_LSFT);
-  tap_code(KC_TAB);
-  unregister_code(KC_LSFT);
 }
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-  return is_thumb_tap_hold(keycode);
+  if (is_target_home_row_hold_key(keycode)) {
+    return false;
+  }
+
+  return true;
 }
 
 uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_keycode) {
-  if (is_thumb_tap_hold(keycode)) {
-    return 0;
+  if (is_target_home_row_hold_key(keycode)) {
+    return 50;
   }
 
-  if (is_home_row_mod(keycode)) {
-    return FLOW_TAP_TERM;
-  }
-
-  return 0;
-}
-
-void matrix_scan_user(void) {
-  static bool initial_repeat_done = false;
-
-  if (app_switch_forward_held || app_switch_backward_held) {
-    uint16_t elapsed = timer_elapsed(app_switch_timer);
-
-    if (!initial_repeat_done) {
-      if (elapsed >= APP_SWITCH_INITIAL_DELAY) {
-        if (app_switch_forward_held) {
-          app_switcher_step_forward();
-        } else if (app_switch_backward_held) {
-          app_switcher_step_backward();
-        }
-        app_switch_timer = timer_read();
-        initial_repeat_done = true;
-      }
-    } else {
-      if (elapsed >= APP_SWITCH_REPEAT_DELAY) {
-        if (app_switch_forward_held) {
-          app_switcher_step_forward();
-        } else if (app_switch_backward_held) {
-          app_switcher_step_backward();
-        }
-        app_switch_timer = timer_read();
-      }
-    }
-  } else {
-    initial_repeat_done = false;
-  }
+  return FLOW_TAP_TERM;
 }
 
 bool custom_process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case KC_F16:
       if (record->event.pressed) {
-        app_switch_forward_held = true;
-        app_switch_backward_held = false;
-        app_switcher_start();
-        app_switcher_step_forward();
-        app_switch_timer = timer_read();
+        register_code(KC_LGUI);
+        tap_code(KC_TAB);
       } else {
-        app_switch_forward_held = false;
-        app_switcher_stop();
+        unregister_code(KC_LGUI);
       }
       return false;
 
     case KC_F17:
       if (record->event.pressed) {
-        app_switch_backward_held = true;
-        app_switch_forward_held = false;
-        app_switcher_start();
-        app_switcher_step_backward();
-        app_switch_timer = timer_read();
+        register_code(KC_LGUI);
+        register_code(KC_LSFT);
+        tap_code(KC_TAB);
       } else {
-        app_switch_backward_held = false;
-        app_switcher_stop();
+        unregister_code(KC_LSFT);
+        unregister_code(KC_LGUI);
       }
       return false;
 
@@ -138,6 +70,7 @@ bool custom_process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         register_code(KC_LGUI);
         tap_code(KC_GRV);
+      } else {
         unregister_code(KC_LGUI);
       }
       return false;
@@ -147,6 +80,7 @@ bool custom_process_record_user(uint16_t keycode, keyrecord_t *record) {
         register_code(KC_LGUI);
         register_code(KC_LSFT);
         tap_code(KC_GRV);
+      } else {
         unregister_code(KC_LSFT);
         unregister_code(KC_LGUI);
       }
