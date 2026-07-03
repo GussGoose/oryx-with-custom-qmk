f3338e5b05e83c1c133cc552769b979b7cf4d4f0
Gustavo Garcia <gustavogs2811@gmail.com>
2026-07-03 13:18:25 -0600

    Update config.h

--- custom/voyager/config.h
+++ custom/voyager/config.h
@@ -11,7 +11,7 @@
 #define RGB_MATRIX_VAL_STEP 24
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "bqbpg/EeaXpQ"
+#define SERIAL_NUMBER "bqbpg/9DW4gW"
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
